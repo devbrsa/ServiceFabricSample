e8622b09a06e96ed33b625cb7e1cfb50f50463df
Bruno Arruda <arruda@outlook.com>
2018-07-30 16:31:19 +0200

    Add project files.

---
+++ ECommerce.API/Model/ApiProduct.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ECommerce.API.Model
+{
+    public class ApiProduct
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("price")]
+        public double Price { get; set; }
+
+        [JsonProperty("isAvailable")]
+        public bool IsAvailable { get; set; }
+    }
+}
